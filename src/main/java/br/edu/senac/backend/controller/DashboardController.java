fdbd70ab2c918d3fe26e2e9b38a712021e471efe
gustavokoichi <guga_koichi@hotmail.com>
2026-04-29 19:28:16 -0300

    feat: Ajuste no DashboardController.java retirando a injeção manual do constructor e adicionando anotation do lombok.

--- src/main/java/br/edu/senac/backend/controller/DashboardController.java
+++ src/main/java/br/edu/senac/backend/controller/DashboardController.java
@@ -2,6 +2,7 @@ package br.edu.senac.backend.controller;
 
 import br.edu.senac.backend.dto.DashboardAlunoResponse;
 import br.edu.senac.backend.service.DashboardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
 
     private final DashboardService dashboardService;
+
     @GetMapping("/aluno/{alunoId}/curso/{cursoId}")
     @PreAuthorize("hasAnyRole('ALUNO','COORDENADOR','SUPER_ADMIN')")
     public ResponseEntity<DashboardAlunoResponse> dashboardAluno(
